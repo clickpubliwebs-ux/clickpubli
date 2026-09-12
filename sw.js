55f6b0e6a803db94c6e6c2f590c31f0c4d519d61
clickpubliwebs-ux <clickpubliwebs@gmail.com>
2026-09-12 15:17:08 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,21 @@
-   const CACHE = "clickpubli-v1";
-   self.addEventListener("install", e => {
-       e.waitUntil(caches.open(CACHE).then(c => c.addAll(["/", "/index.html", "/manifest.webmanifest", "/icon.svg"])));
-   });
-   self.addEventListener("fetch", e => {
-       e.respondWith(
-           caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
-               const copy = res.clone();
-               if (e.request.method === "GET" && res.ok) caches.open(CACHE).then(c => c.put(e.request, copy));
-               return res;
-           }).catch(() => caches.match("/index.html")))
-       );
-   });
+const CACHE = "clickpubli-v2";
+self.addEventListener("install", e => {
+    self.skipWaiting();
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(["/", "/index.html", "/manifest.webmanifest", "/icon.svg"])));
+});
+self.addEventListener("activate", e => {
+    e.waitUntil(
+        caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+            .then(() => self.clients.claim())
+    );
+});
+self.addEventListener("fetch", e => {
+    if (e.request.method !== "GET") return;
+    e.respondWith(
+        fetch(e.request).then(res => {
+            const copy = res.clone();
+            if (res.ok) caches.open(CACHE).then(c => c.put(e.request, copy));
+            return res;
+        }).catch(() => caches.match(e.request).then(hit => hit || caches.match("/index.html")))
+    );
+});
